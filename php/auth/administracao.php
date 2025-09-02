323ceae72a0cd105b1af96677cfbbf881c578c08
JAndrade912 <joao.andrade91@aluno.ce.gov.br>
2025-09-02 11:25:04 -0300

    administração.php

--- php/auth/administracao.php
+++ php/auth/administracao.php
@@ -1,36 +1,40 @@
 <?php
 
-    require_once 'connect.php';
+require_once 'connect.php';
 
-    class Admin {
+class Admin{
 
-        private $db;
+    private $db;
 
-        public function __construct() {
-            $database = new database();
-            $this -> db = $database -> connect();
-            session_start();
-        }
-
-        public function login ($username,$password){
-            $stmt = $this -> db -> prepare("SELECT * FROM Administracao WHERE username = ?");
-            $stmt -> execute([$username]);
-            if($stmt -> rowCount() === 1){
-                $admin = $stmt -> fetch(PDO::FETCH_ASSOC);
-                if(password_verify($password,$admin['password'])){
-                    $_SESSION['admin'] = ['id' => $admin['id'], 'username' => $admin['username']];
-                    return true; 
-                }
-            }
-            return false;
-        }
-
-        public function isLoged(){
-            return isset ($_SESSION['admin']);
-        }
-
-        public function logout(){
-            unset($_SESSION['admin']);
-            session_destroy();
-        }
+    public function __construct()
+    {
+        $database = new database();
+        $this->db = $database->connect();
+        session_start();
     }
+
+    public function login($username, $password)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM Administracao WHERE username = ?");
+        $stmt->execute([$username]);
+        if ($stmt->rowCount() === 1) {
+            $admin = $stmt->fetch(PDO::FETCH_ASSOC);
+            if (password_verify($password, $admin['password'])) {
+                $_SESSION['admin'] = ['id' => $admin['id'], 'username' => $admin['username']];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function isLoged()
+    {
+        return isset($_SESSION['admin']);
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['admin']);
+        session_destroy();
+    }
+}
