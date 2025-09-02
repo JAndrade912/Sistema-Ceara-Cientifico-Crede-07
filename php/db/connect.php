5dc8da74ec74faacd5849a5b32b8051284275bfe
JAndrade912 <joao.andrade91@aluno.ce.gov.br>
2025-09-02 09:04:15 -0300

    Banco de dados em Orientação a Objetos

--- php/db/connect.php
+++ php/db/connect.php
@@ -1,13 +1,20 @@
 <?php
 
-    $host = "localhost";
-    $usuario = "root";
-    $senha = "cr701201";
-    $db = "SACC";
+class Database {
+    private $host = "localhost";
+    private $dbname = "SACC";
+    private $username = "root";
+    private $password = "cr701201";
+    public $conn;
 
-    $conn = mysqli_connect($host,$usuario,$senha,$db);
-
-    if($conn -> error){
-        die("Não foi possível conectar ao banco de dados");
+    public function connect()
+    {
+        try {
+            $this -> conn = new PDO("mysql:host={$this -> host};dbname={$this -> dbname};charset=utf8", $this -> username, $this -> password);
+            $this -> conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this -> conn;
+        } catch (PDOException $e) {
+            die('Erro na conexão: ' . $e->getMessage());
+        }
     }
-?>
+}
