a5bcc2f0918134c87e49554b791c740e357e728f
JAndrade912 <joao.andrade91@aluno.ce.gov.br>
2025-09-04 11:04:08 -0300

    Login dos jurados terminado e funcionando

--- html/login_jurado.php
+++ html/login_jurado.php
@@ -1,5 +1,16 @@
+<?php
+session_start();
+$error = '';
+
+if (isset($_SESSION['login_error'])) {
+  $error = $_SESSION['login_error'];
+  unset($_SESSION['login_error']);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,46 +21,55 @@
   <link rel="stylesheet" href="../boostrap/CSS/bootstrap.min.css">
   <script src="../boostrap/JS/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
   <nav class="navbar bg-body-tertiary">
     <div class="d-flex">
       <a class="navbar-brand" href="#">
-        <div id="corpo-img"> 
+        <div id="corpo-img">
           <img src="../assets/img/SACC.png" alt="Logo" id="img-sacc" class="d-inline-block align-text-top">
         </div>
       </a>
     </div>
   </nav>
   <div class="container d-flex justify-content-center align-items-center">
-    
+
     <div class="container d-flex justify-content-center align-items-center">
-    <div class="container-fluid" id="login-box">
-      <div class="right">
-      <div class="login-box">
-        <div class="login-cont">
-          <h2><b>Jurado</b></h2><br>
-          <form method="POST" action="" id="loginForm">
-            <div class="form-group">
-              <label for="usuario"><b>Usuário:</b></label>
-              <br>
-              <input type="text" name="usuario" id="usuario" />
-            </div>
-              <br>
-            <div class="form-group">
-              <label for="password"><b>Senha:</b></label>
+      <div class="container-fluid" id="login-box">
+        <div class="right">
+          <div class="login-box">
+            <div class="login-cont">
+              <h2><b>Jurado</b></h2><br>
+              <form method="POST" action="../php/jurado.php" id="loginForm">
+                <div class="form-group">
+                  <label for="usuario"><b>Usuário:</b></label>
+                  <br>
+                  <input type="text" name="usuario" id="usuario" />
+                </div>
                 <br>
-              <input type="password" name="password" id="password" required placeholder="Digite sua senha"/>
+                <div class="form-group">
+                  <label for="senha"><b>Senha:</b></label>
+                  <br>
+                  <input type="password" name="senha" id="senha" required placeholder="Digite sua senha" />
+                </div>
+                <button type="submit" id="bt1">Entrar</button>
+              </form>
+
+              <?php if (!empty($error)) : ?>
+                <div style="color: red; margin-top: 10px; text-align: center;">
+                  <?= htmlspecialchars($error) ?>
+                </div>
+              <?php endif; ?>
+
             </div>
-            <button type="submit" id="bt1">Entrar</button>
-          </form>
+          </div>
+        </div>
+        <div class="logos">
+          <img src="../assets/img/crede7.png" alt="Crede 7 Canindé">
         </div>
       </div>
     </div>
-      <div class="logos">
-        <img src="../assets/img/crede7.png" alt="Crede 7 Canindé">
-      </div>
-    </div>
-  </div>
   </div>
 </body>
+
 </html>
---
+++ php/jurado.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require_once '../php/connect.php';
+
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
+
+    try {
+        $stmt = $pdo->prepare("SELECT * FROM Jurados WHERE usuario = :usuario LIMIT 1");
+        $stmt->execute([':usuario' => $usuario]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && $senha == $user['senha']) {
+            $_SESSION['id_admin'] = $user['id_jurados'];
+            $_SESSION['usuario'] = $user['usuario'];
+            $_SESSION['senha'] = $user['senha'];
+
+            header('Location: ../html/jurado-real.php');
+            exit();
+        } else {
+            $_SESSION['login_error'] = 'Email e/ou senha errados!';
+            header('Location: ../html/login_jurado.php');
+            exit();
+        }
+    } catch (PDOException $e) {
+        $error = 'Erro ao tentar fazer login' . $e->getMessage();
+    }
+}
