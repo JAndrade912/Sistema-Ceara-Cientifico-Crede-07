8d2c99cb349400b4be49663c4d09d309ac343fde
JAndrade912 <joao.andrade91@aluno.ce.gov.br>
2025-09-05 11:00:39 -0300

    Cadastro das escolas quase completo

--- php/Cadescola.php
+++ php/Cadescola.php
@@ -3,4 +3,18 @@ require_once '../php/connect.php';
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $nome = $_POST['nome'] ?? '';
+    $focalizado = isset($_POST['focalizada']) ? (int)$_POST['focalizada'] : 0;
+    $ide = $_POST['ide'] ?? null;
+    $municipio = $_POST['municipio'] ?? 'Desconhecido';
+
+    if(empty($nome)){
+        die('Nome da escola é obrigatório!');
+    }
+
+    $stmt = $pdo -> prepare("INSERT INTO Escolas (nome, focalizada, ide, municipio) VALUES (?, ?, ?, ?)");
+    try{
+        $stmt -> execute([$nome,$focalizado,$ide,$municipio]);
+    }catch(PDOException $e){
+        die('Erro ao cadastrar escola' . $e -> getMessage());
+    }
 }
