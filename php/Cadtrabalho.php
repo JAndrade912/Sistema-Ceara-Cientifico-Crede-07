a0d748abaed160e2d023e68af8631eaad3439b91
HannaLavine <138686617+camillelimade@users.noreply.github.com>
2025-09-10 09:46:25 -0300

    Função de Cadastro de trabalhos adicionada

--- php/Cadtrabalho.php
+++ php/Cadtrabalho.php
@@ -2,7 +2,7 @@
 require_once '../php/connect.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $titulo = $_POST['titulo'];
-    $observacoes = $_POST['observacoes'] ?? null;
+    $observacoes = $_POST['observacoes'] ?? '.';
     $id_escolas = $_POST['escola'] ?? null;
     $id_jurados = $_POST['id_jurados'] ?? null;
     $id_area = $_POST['area'] ?? null;
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     try {
         $pdo->beginTransaction();
 
-        $stmt = $pdo->prepare("INSERT INTO Trabalhos (titulo, observacoes, id_escolas, id_jurados, id_area, id_categoria) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt = $pdo->prepare("INSERT INTO Trabalhos (titulo, observacoes, id_escolas, id_jurados, id_areas, id_categoria) VALUES (?, ?, ?, ?, ?, ?)");
         $stmt->execute([$titulo, $observacoes, $id_escolas, $id_jurados, $id_area, $id_categoria]);
         $pdo->commit();
         header('Location: ../html/admin-dashboard.php?msg=sucesso');
